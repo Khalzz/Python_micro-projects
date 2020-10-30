cf6986a0d94383626a94ae6a9c775cec97790b6c
Rodrigo Seguel <63627567+Khalzz@users.noreply.github.com>
2020-10-29 22:54:49 -0300

    Update Copiar.py

--- Micro-Projectos py/Creador-De-Respaldos/Copiar.py
+++ Micro-Projectos py/Creador-De-Respaldos/Copiar.py
@@ -1,16 +1,22 @@
-
 import shutil #nos permite interactuar con carpetas de forma avanzada
+import os
 import time
 
-ruta ='ingresa/la/carpeta/a/copiar'
-rutaCopia ='ingresa/la/carpeta/donde/ira/el/respaldo'
+ruta ='/home/khals/Desktop/ProjectAlone'
+rutaCopia = '/home/khals/Desktop/DevShit/Respaldo'
 
-time.sleep(0.3)
-print("Eliminando carpeta original")
-shutil.rmtree(rutaCopia)
-print("La carpeta original se ha eliminado")
+def crearCarpeta():
+    time.sleep(0.3)
+    print("Eliminando carpeta original")
+    shutil.rmtree(rutaCopia)
+    print("La carpeta original se ha eliminado")
+    time.sleep(0.3)
+    print("Se esta generando el respaldo")
+    shutil.copytree(ruta,rutaCopia)
+    print("Finalizado")
 
-time.sleep(0.3)
-print("Se esta generando el respaldo")
-shutil.copytree(ruta,rutaCopia)
-print("Finalizado")
+if os.path.isdir(rutaCopia): #si existe carpeta "ruta copia" crear respaldo
+    crearCarpeta()
+else: #si la carpeta no existe, crear carpeta y hacer respaldo
+    os.mkdir(rutaCopia)
+    crearCarpeta()
